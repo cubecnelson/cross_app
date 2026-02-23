3a3579c959bcfa934787747a217cf3c0833610e5
Nelson Cheung <nelson.cheung@livibank.com>
2026-02-23 16:50:49 +0800

    oth: fix screenshot

--- screenshot_capture.sh
+++ screenshot_capture.sh
@@ -47,7 +47,6 @@ CAPTURE_SCREENSHOTS=true SCREENSHOT_MODE=true flutter test \
   --dart-define=CAPTURE_SCREENSHOTS=true \
   --dart-define=SCREENSHOT_MODE=true \
   --no-track-widget-creation \
-  --no-sound-null-safety \
   --platform chrome \
   --timeout 60s
 
